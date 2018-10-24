3b6f28f49487bf347f2510a5584dacf642e7f2d4
Peter Zagubisalo <peter.zagubisalo@gmail.com>
2018-10-24 23:26:36 +0700

    Update upload.bat

--- upload.bat
+++ upload.bat
@@ -1,8 +1,6 @@
 ::conda install -c defaults -c conda-forge twine
-set "script_dir=%~dp0"
-cd /d "%script_dir%"
+cd /d "%~dp0"
 
 python setup.py sdist
-chcp 1252 && set "PYTHONIOENCODING="
 twine upload dist/* --skip-existing
 chcp 65001 && set "PYTHONIOENCODING=utf-8"
